b803b54a0d346f456edb24e68e0f81516a04c513
Jaron <jjkenyon777@gmail.com>
2021-08-14 13:50:05 -0700

    added generate password function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 // Assignment code here
-
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var special = "!#$%&'()*+,-./:;<=>?@[]\^_`{|}~";
+var numbers = "123456789";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -12,6 +15,54 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generatePassword() {
+  var length = window.prompt("How many characters do you want in your password?");
+  if (length <= 128 && length >= 8 ){
+    var confirmLower = window.confirm("Do you want lower case letters?");
+    var confirmUpper = window.confirm("Do you want upper case letters?");
+    var confirmNumber = window.confirm("Do you want numbers?");
+    var confirmSpecial = window.confirm("do you want special characters?");
+    
+    var charSet = "";
+    var password = "";
+
+    if (confirmLower === true) {
+      charSet += lowerCase ;
+    }
+    if (confirmUpper === true) {
+      charSet += upperCase ;
+    }
+    if (confirmNumber === true) {
+      charSet += numbers ;
+    }
+    if (confirmSpecial === true) {
+      charSet += special ;
+    }
+
+    for (var i = 0; i < length; i++) {
+      password += charSet.charAt(Math.floor(Math.random() * charSet.length));
+    }
+    return (password);
+  }
+    else {
+      window.alert("Please enter a number between 8-128");
+
+      return;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
